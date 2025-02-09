7bb0ad946ca7d2889c044673dafde0304ab07b78
Majuyi <majuyitan@gmail.com>
2025-02-09 18:08:47 +0100

    Modify index.js

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ const cors = require('cors');
 const classifyNumRoute = require('./src/route/classifyNumberRoutes');
 
 const app = express();
+const PORT = 3000;
 
-app.use(cors({ origin: "*" }));
+app.use(cors({
+	origin: "*"
+}));
 app.use(express.json());
 
-app.use('/api', classifyNumRoute);
+app.use('/', classifyNumRoute);
 
-module.exports = app;
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`)
+});
